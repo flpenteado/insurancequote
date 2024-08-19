8853d809f38c0ee072362b62a3ab07295455b4d5
Fabio Penteado <fabio@penteado.io>
2024-08-19 11:12:56 -0300

    chore: initial commit

---
+++ src/main/java/io/acme/insurancequote/InsurancequoteApplication.java
@@ -0,0 +1,13 @@
+package io.acme.insurancequote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancequoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancequoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/acme/insurancequote/InsurancequoteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acme.insurancequote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancequoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
